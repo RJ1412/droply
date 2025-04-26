83432cac0bbe6bc9129706bea11fc60e5266228d
Rahul Raj <rahulrajdec65@gmail.com>
2025-04-26 20:10:08 +0530

    Database Connected

---
+++ lib/db/schema.ts
@@ -0,0 +1,44 @@
+import {pgTable , text , uuid ,boolean, integer , timestamp} from "drizzle-orm/pg-core"
+import {relations} from "drizzle-orm"
+import { time } from "console"
+import { Children } from "react"
+
+export const files = pgTable("files" , {
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: text("name").notNull(),
+    path: text("path").notNull(),
+    size: integer("size").notNull(),
+    type: text("type").notNull(),
+
+    fileUrl : text("file_url").notNull(), // url to access file
+    thumbnailUrl : text("thumbnail_url"),
+
+
+    userId: text("user_id").notNull(),
+    parentId : uuid("parennt_id"),
+    
+
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt : timestamp("updated_at").defaultNow().notNull()
+})
+
+/*
+parent : each file/folder can have one parent folder
+
+child : each file/folder can have many child files/folder
+*/
+
+export const fileRealtions = relations(files , ({one , many}) => ({
+    parent: one(files , {
+        fields: [files.parentId],
+        references : [files.id]
+    }),
+
+    children: many(files)
+}))
+
+export const File = typeof files.$inferSelect
+export const NewFile = typeof files.$inferInsert
