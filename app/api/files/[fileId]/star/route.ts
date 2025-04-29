6fc8b983a91cb757f9e94dfd7b061d11f86df4bc
Rahul Raj <rahulrajdec65@gmail.com>
2025-04-29 23:12:54 +0530

    Created Starred route

---
+++ app/api/files/[fileId]/star/route.ts
@@ -0,0 +1,64 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { error } from "console";
+import { and, eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+
+export async function PATCH(
+    request: NextResponse,
+    props: { params: Promise<{ fileId: string }> }
+) {
+    try {
+        const { userId } = await auth()
+        if (!userId) {
+            return NextResponse.json(
+                { error: "Unauthorized" },
+                { status: 401 }
+            )
+        }
+
+        const { fileId } = await props.params
+
+        if (!fileId) {
+            return NextResponse.json(
+                { error: "File id is required" },
+                { status: 401 }
+            );
+        }
+
+        const [file] = await db.select().from(files).where(
+            and(
+                eq(files.id, fileId),
+                eq(files.userId, userId)
+            )
+        )
+
+
+        if (!file) {
+            return NextResponse.json(
+                { error: "File not found" },
+                { status: 401 }
+            );
+        }
+
+       const updatedFiles = await db.update(files).set({isStarred : !file.isStarred}).where(
+            and(
+                eq(files.id, fileId),
+                eq(files.userId, userId)
+            )
+        ).returning();
+
+        console.log(updatedFiles);
+
+        const updatedFile = updatedFiles[0]
+        return NextResponse.json(updatedFile)
+        
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+          );
+    }
+}
